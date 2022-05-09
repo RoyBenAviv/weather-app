b53aca05e0e2b50ebd9c29c8d0d5e7ea0d570491
Roy Ben Aviv <roybenavivv@gmail.com>
2022-05-09 14:49:30 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './styles/style.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
